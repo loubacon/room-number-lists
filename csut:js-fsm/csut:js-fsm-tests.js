b8e449426e618bf39e215cf4c4ae39ae1f0fe342
loubacon <chris.sutela+loubacon@gmail.com>
2015-03-21 14:44:56 +0000

    csut:js-fsm: more tests

--- csut:js-fsm/csut:js-fsm-tests.js
+++ csut:js-fsm/csut:js-fsm-tests.js
@@ -12,3 +12,74 @@ Tinytest.add('fsm_1', function (test) {
   ]});
   test.equal(fsm.current, 'green');
 });
+
+Tinytest.add('fsm_2', function (test) {
+  var fsm = StateMachine.create({
+    initial: 'green',
+    events: [
+      { name: 'warn',  from: 'green',  to: 'yellow' },
+      { name: 'panic', from: 'yellow', to: 'red'    },
+      { name: 'calm',  from: 'red',    to: 'yellow' },
+      { name: 'clear', from: 'yellow', to: 'green'  }
+  ]});
+  fsm.warn();
+  test.equal(fsm.current, 'yellow');
+});
+
+Tinytest.add('fsm_3', function (test) {
+  var fsm = StateMachine.create({
+    initial: 'green',
+    events: [
+      { name: 'warn',  from: 'green',  to: 'yellow' },
+      { name: 'panic', from: 'yellow', to: 'red'    },
+      { name: 'calm',  from: 'red',    to: 'yellow' },
+      { name: 'clear', from: 'yellow', to: 'green'  }
+  ]});
+  fsm.warn();
+  fsm.panic();
+  test.equal(fsm.current, 'red');
+});
+Tinytest.add('fsm_4', function (test) {
+  var fsm = StateMachine.create({
+    initial: 'green',
+    events: [
+      { name: 'warn',  from: 'green',  to: 'yellow' },
+      { name: 'panic', from: 'yellow', to: 'red'    },
+      { name: 'calm',  from: 'red',    to: 'yellow' },
+      { name: 'clear', from: 'yellow', to: 'green'  }
+  ]});
+  fsm.warn();
+  fsm.panic();
+  fsm.calm();
+  test.equal(fsm.current, 'yellow');
+});
+
+Tinytest.add('fsm_5', function (test) {
+  var fsm = StateMachine.create({
+    initial: 'green',
+    events: [
+      { name: 'warn',  from: 'green',  to: 'yellow' },
+      { name: 'panic', from: 'yellow', to: 'red'    },
+      { name: 'calm',  from: 'red',    to: 'yellow' },
+      { name: 'clear', from: 'yellow', to: 'green'  }
+  ]});
+  fsm.warn();
+  fsm.panic();
+  fsm.calm();
+  fsm.clear();
+  test.equal(fsm.current, 'green');
+});
+
+Tinytest.add('fsm_6', function (test) {
+  var fsm = StateMachine.create({
+    initial: 'green',
+    events: [
+      { name: 'warn',  from: 'green',  to: 'yellow' },
+      { name: 'panic', from: 'yellow', to: 'red'    },
+      { name: 'calm',  from: 'red',    to: 'yellow' },
+      { name: 'clear', from: 'yellow', to: 'green'  }
+  ]});
+  fsm.warn();
+  fsm.clear();
+  test.equal(fsm.current, 'green');
+});
